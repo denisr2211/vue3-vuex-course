21e746d5e05fa073d39cf8ebd357f73160c45568
Denis <denis.r2211@gmail.com>
2023-01-08 15:50:31 +0200

    first commit

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,41 @@
+<template>
+    <select v-model="localValue" @change="changeOption">
+        <option disabled value="">Выберите из списка</option>
+        <option 
+            v-for="option in options"
+            :key="option.value"
+            :value="option.value"
+        >
+            {{ option.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'my-select',
+    props: {
+        modelValue: {
+            type: String
+        },
+        options: {
+            type: Array,
+            default: () => []
+        }
+    },
+    methods: {
+        changeOption(event) {
+            this.$emit('update:modelValue', event.target.value)
+        }
+    },
+    data(){
+        localValue:String
+        
+    
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
